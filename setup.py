0f988517d468660a845073fdf89299639c9f6e6b
Camillo Visini <c.visini@gmail.com>
2020-11-02 18:10:35 +0100

    Add cffi to setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
-# python3 setup.py py2app
-
+import os
 from setuptools import setup
 
-APP = ['timebox.py']
+VERSION = "0.0.1"
+if "VERSION" in os.environ:
+    VERSION = os.getenv("VERSION")
+
+
+APP_NAME = "App"
+if "APP_NAME" in os.environ:
+    APP_NAME = os.getenv("APP_NAME")
+
+
+APP = ["main.py"]
 DATA_FILES = []
 OPTIONS = {
-    'argv_emulation': True,
-    'iconfile': 'icon.icns',
-    'plist': {
-        'CFBundleShortVersionString': '0.2.0',
-        'LSUIElement': True,
+    "argv_emulation": True,
+    "iconfile": "icon.icns",
+    "plist": {
+        "CFBundleShortVersionString": VERSION,
+        "LSUIElement": True,
     },
-    'packages': ['rumps'],
+    "packages": ["rumps", "paramiko", "cffi"],
 }
 
 setup(
     app=APP,
-    name='Timebox',
+    name=APP_NAME,
     data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
 )
